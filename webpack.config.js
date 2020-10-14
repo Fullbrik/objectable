b17c87cf176ca8ac1c29e6f2cf41ecbf65657d1b
Fullbrik <40151628+Fullbrik@users.noreply.github.com>
2020-10-14 16:52:32 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,48 @@
-path = require('path');
+path = require("path");
 
-module.exports = {
-  entry: "./src/index.ts",
-  module: {
-    rules: [
-      {
-        test: /^((?!spec).)*\.ts?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
-  },
-  output: {
-    filename: "index.js",
-    path: path.resolve('./dist'),
-    library: 'Objectable',
-    libraryTarget: 'umd'
-  },
-};
+module.exports = [
+	{
+		name: "npm",
+		entry: "./src/index.ts",
+		module: {
+			rules: [
+				{
+					test: /^((?!spec).)*\.ts?$/,
+					use: "ts-loader",
+					exclude: /node_modules/,
+				},
+			],
+		},
+		resolve: {
+			extensions: [".tsx", ".ts", ".js"],
+		},
+		output: {
+			filename: "index.js",
+			path: path.resolve("./dist"),
+			library: "Objectable",
+			libraryTarget: "umd",
+		},
+	},
+	{
+		name: "cdn",
+		entry: "./src/index.ts",
+		module: {
+			rules: [
+				{
+					test: /^((?!spec).)*\.ts?$/,
+					use: "ts-loader",
+					exclude: /node_modules/,
+				},
+			],
+		},
+		resolve: {
+			extensions: [".tsx", ".ts", ".js"],
+		},
+		output: {
+			filename: "index.cdn.js",
+			path: path.resolve("./dist"),
+			library: "Objectable",
+			libraryTarget: "var",
+		},
+	},
+];
