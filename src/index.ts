249328259642d6ca0b6665f3a88bb46973c63acb
Fullbrik <40151628+Fullbrik@users.noreply.github.com>
2020-10-14 16:18:15 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,37 @@
-import { addComponents, AdditionalParams, Component, ProtoComponent } from "./component";
-import { AddPropTransform } from './html'; 
+import {
+	addComponents,
+	AdditionalParams,
+	Component,
+	ProtoComponent,
+} from "./component";
+import { AddPropTransform } from "./html";
 
-export {ProtoComponentParams, ProtoComponent, ComponentParams, Component} from './component';
+export {
+	ProtoComponentParams,
+	ProtoComponent,
+	ComponentParams,
+	Component,
+} from "./component";
 
-module.exports = {
-  render: (component: ProtoComponent) => {
-    var comp = new Component(component, 'Root');
-    return comp.render(comp);
-  },
-  addComponents: addComponents,
-  additionalParams: AdditionalParams,
-  component: function (name?: string) {
-    return function (
-      target: any,
-      propertyKey: string,
-      descriptor: PropertyDecorator
-    ) {
-      addComponents({ [name ?? propertyKey]: target });
-    };
-  },
-  addHTMLPropertyTransform: AddPropTransform,
-};
+export function render(component: ProtoComponent) {
+	var comp = new Component(component, "Root");
+	return comp.render(comp);
+}
+
+export { addComponents } from "./component";
+
+export var additionalParams = AdditionalParams;
+
+export function component(name?: string) {
+	return function (
+		target: any,
+		propertyKey: string,
+		descriptor: PropertyDecorator
+	) {
+		addComponents({ [name ?? propertyKey]: target });
+	};
+}
+
+export var addHTMLPropertyTransform = AddPropTransform;
 
 // module.exports = Objectable;
